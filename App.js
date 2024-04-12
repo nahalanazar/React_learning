74f531e19e79732ba99a76366fe585b86a9c8a01
nasheed.sm <neshinasersm@gmail.com>
2024-04-12 12:23:38 +0530

    React functionalComponent

--- App.js
+++ App.js
@@ -3,24 +3,38 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", { id: "title" }, "Heading 1")
+// react element: It is an object 
+const element1 = React.createElement("h1", { id: "title", key: "1" }, "Heading Element")
 
-const heading2 = React.createElement(
-    "h2",
-    {
-        id: "subtitle"
-    },
-    "Heading 2 Parcel"
+// react element using jsx: (a variable in js)
+const element2 = (
+    <h1 id="h1">Good Morning</h1>
 )
 
-const container = React.createElement(
-    "div",
-    {
-        id:"container"
-    },
-    [heading, heading2]
+// react component
+const ChildComponent = () => (
+    <p>I am a component</p>
 )
+
+// react component: It is a function
+
+// component composition: component inside a component
+const Component = () => {
+    return (
+        <div>
+            {console.log("any js code")}
+            {element1}
+            <ChildComponent />
+            {ChildComponent()}
+            <h1>Hello</h1>
+        </div>
+    );
+}
     
 const root = ReactDOM.createRoot(document.getElementById('root'))
     
-root.render(container)
+// rendering react element: root.render(element1)
+
+// rendering react component:
+
+root.render(<Component />)
